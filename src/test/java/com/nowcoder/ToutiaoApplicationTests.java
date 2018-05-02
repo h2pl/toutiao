725251d56ca1895c1990e0edc2a0fe743c831e9f
724888 <362294931@qq.com>
2018-05-02 11:24:16 +0800

    toutiao config

---
+++ src/test/java/com/nowcoder/ToutiaoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.nowcoder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ToutiaoApplication.class)
+@WebAppConfiguration
+public class ToutiaoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
