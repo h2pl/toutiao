33576913a00a1c7d029106c76762fa707ad003b2
724888 <362294931@qq.com>
2018-05-08 16:06:00 +0800

    redis实现异步消息队列，单线程循环处理事件。 新增异步请求处理包和工具类，专门用来处理异步事件，比如点赞，登录异常等。 新增邮件发送功能，用户密码已被我删掉。

---
+++ src/main/java/com/nowcoder/util/MailSender.java
@@ -0,0 +1,74 @@
+package com.nowcoder.util;
+
+import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+import org.apache.velocity.app.VelocityEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.velocity.VelocityProperties;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeUtility;
+
+import org.springframework.ui.velocity.VelocityEngineFactoryBean;
+import org.springframework.ui.velocity.VelocityEngineUtils;
+
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Created by nowcoder on 2016/7/15. // course@nowcoder.com NKnk66
+ */
+@Service
+public class MailSender implements InitializingBean {
+    private static final Logger logger = LoggerFactory.getLogger(MailSender.class);
+    private JavaMailSenderImpl mailSender;
+
+    @Autowired
+    private VelocityEngine velocityEngine;
+
+    public boolean sendWithHTMLTemplate(String to, String subject,
+                                        String template, Map<String, Object> model) {
+        try {
+            String nick = MimeUtility.encodeText("牛客中级课");
+            InternetAddress from = new InternetAddress("dasdas");
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+            String result = VelocityEngineUtils
+                    .mergeTemplateIntoString(velocityEngine, template, "UTF-8", model);
+            mimeMessageHelper.setTo(to);
+            mimeMessageHelper.setFrom(from);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(result, true);
+            mailSender.send(mimeMessage);
+            return true;
+        } catch (Exception e) {
+            logger.error("发送邮件失败" + e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        mailSender = new JavaMailSenderImpl();
+
+        // 请输入自己的邮箱和密码，用于发送邮件
+        mailSender.setUsername("asdasd");
+        mailSender.setPassword("dasdas");
+        mailSender.setHost("smtp.exmail.qq.com");
+        // 请配置自己的邮箱和密码
+
+        mailSender.setPort(465);
+        mailSender.setProtocol("smtps");
+        mailSender.setDefaultEncoding("utf8");
+        Properties javaMailProperties = new Properties();
+        javaMailProperties.put("mail.smtp.ssl.enable", true);
+        mailSender.setJavaMailProperties(javaMailProperties);
+    }
+}
