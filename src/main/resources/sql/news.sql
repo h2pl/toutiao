8da792cd6a0d40d300d59e763685d5b136ad6a68
ZoeShaw101 <1141195807@qq.com>
2018-08-01 23:22:27 +0800

    修改sql错误

--- src/main/resources/sql/news.sql
+++ src/main/resources/sql/news.sql
@@ -1,7 +1,7 @@
-DROP TABLE IF EXISTS 'news';
-CREATE TABLE 'news'(
-	`id` int(11) unsigned NOT NULL AUTO_INCREAMENT,
-	`titie` varchar(128) NOT NULL DEFAULT '',
+DROP TABLE IF EXISTS news;
+CREATE TABLE news(
+	`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+	`title` varchar(128) NOT NULL DEFAULT '',
 	`link` varchar(256) NOT NULL DEFAULT '',
 	`image` varchar(256) NOT NULL DEFAULT '',
 	`like_count` int(11) NOT NULL,
@@ -9,4 +9,4 @@ CREATE TABLE 'news'(
 	`create_date` datetime NOT NULL,
 	`user_id` int(11) NOT NULL,
 	PRIMARY KEY (`id`)
-)ENGINE = InnoDB AUTO_INCREAMENT=11 DEFAULT CHARSET=utf8
+)ENGINE = InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8
